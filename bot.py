6ce4d14d0f99ca08f5c4e7049b3c667b78b826f5
Yana Sirina <yana.sirina@gmail.com>
2022-02-26 17:21:32 +0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+from settings import TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
+
