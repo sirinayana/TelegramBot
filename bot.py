03d292ba9ec45b0e32f3e2dff0d1a11123e81f69
Yana Sirina <yana.sirina@gmail.com>
2022-02-26 18:48:51 +0500

    function of sending pictures

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telebot
 from settings import TOKEN
+from requests import get
+from random import choice
+from pictures import pictures
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -7,12 +10,12 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, 'Напиши "привет"')
+    if message.text == "/picture":
+        bot.send_message(message.from_user.id, "Сейчас я отправлютебе дурацкую картинку:)")
+        bot.send_photo(message.chat.id, get(choice(pictures)).content)
+        bot.send_message(message.from_user.id, "Хочешь еще одну? Напиши /picture")
     else:
-        bot.send_message(message.from_user.id, '"Я тебя не понимаю. Напиши "/help."')
+        bot.send_message(message.from_user.id, "Привет, я бот, которые отправляет тупые картинки! Чтобы получить картинку нажми /picture")
 
 
 bot.polling(none_stop=True, interval=0)
